0c658c1d58a04764d63493bc18ac624aa79caf2a
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-01-31 21:18:57 +0500

    security level is increased

--- src/main/java/com/security/security/SecurityConfig.java
+++ src/main/java/com/security/security/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -28,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http)throws Exception{
         http.authorizeRequests()
                 .antMatchers("/user").hasRole("USER")
-                .antMatchers("/admin").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/admin").hasAnyRole("ADMIN")
                 .antMatchers("/supervisor").hasAnyRole("SUPER")
                 .antMatchers("/").permitAll()
                 .and()
