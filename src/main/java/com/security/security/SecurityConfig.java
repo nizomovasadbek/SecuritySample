3f4dbfdddf5fbe3921d713a15f528e75ece8e965
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-01-31 22:00:36 +0500

    Security level has increased

--- src/main/java/com/security/security/SecurityConfig.java
+++ src/main/java/com/security/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -20,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?")
                 .authoritiesByUsernameQuery("SELECT username, authority, authority from authorities WHERE" +
                         " username =?")
-//                .passwordEncoder(new BCryptPasswordEncoder())
+                .passwordEncoder(new BCryptPasswordEncoder())
                 ;
     }
 
