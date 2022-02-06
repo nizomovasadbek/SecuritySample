d6c90dab490560daaec7caa6dc84867f1fcb2981
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-02-06 13:16:33 +0500

    configured

--- src/main/java/com/security/Application.java
+++ src/main/java/com/security/Application.java
@@ -1,4 +1,17 @@
 package com.security;
 
+import com.security.config.MessageWebApplicationInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication(exclude = FreeMarkerAutoConfiguration.class)
+@Import({MessageWebApplicationInitializer.class})
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
