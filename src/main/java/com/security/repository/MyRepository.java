85eac09a2dd632580e95438e00fb4066ef8926a7
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-02-06 14:11:12 +0500

    JDBC status updated

--- src/main/java/com/security/repository/MyRepository.java
+++ src/main/java/com/security/repository/MyRepository.java
@@ -1,4 +1,16 @@
 package com.security.repository;
 
+import com.security.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class MyRepository {
+
+    @Autowired
+    public JdbcTemplate jdbcTemplate;
+
+    public void addSave(User addUser){
+        String sql = "INSERT INTO user(username, password) VALUES (?, ?)";
+        jdbcTemplate.update(sql, addUser.getUsername(), addUser.getPassword());
+    }
 }
