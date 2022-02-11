eedf6e9b5325746dcf4d875e48ee0c40962427fa
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-02-11 17:46:17 +0500

    Added e-mail sender

---
+++ src/main/java/com/security/config/MailConfig.java
@@ -0,0 +1,22 @@
+package com.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+@ComponentScan("com.security")
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(465);
+        mailSender.setUsername("nizomovasadbekjava@gmail.com");
+        mailSender.setPassword("");
+    }
+}
