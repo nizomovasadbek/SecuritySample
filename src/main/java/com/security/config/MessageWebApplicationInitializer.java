92747570f32076564932507bbac28abee663a752
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-02-11 15:54:30 +0500

    Hidden method added

--- src/main/java/com/security/config/MessageWebApplicationInitializer.java
+++ src/main/java/com/security/config/MessageWebApplicationInitializer.java
@@ -1,6 +1,10 @@
 package com.security.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class MessageWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -16,5 +20,16 @@ public class MessageWebApplicationInitializer extends AbstractAnnotationConfigDi
     @Override
     protected String[] getServletMappings() {
         return new String[] { "/" };
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        hiddenHttpMethodFilter(aServletContext);
+    }
+
+    private void hiddenHttpMethodFilter(ServletContext aServletContext){
+        aServletContext.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter())
+                .addMappingForUrlPatterns(null, true, "/*");
     }
 }
