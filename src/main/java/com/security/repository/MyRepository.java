5cac7f1d9966b0ced94433c9c9cdcf604ea2293a
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-02-11 18:23:41 +0500

    Security level has increased

--- src/main/java/com/security/repository/MyRepository.java
+++ src/main/java/com/security/repository/MyRepository.java
@@ -4,6 +4,7 @@ import com.security.model.User;
 import com.security.repository.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,6 +14,10 @@ public class MyRepository {
     public JdbcTemplate jdbcTemplate;
 
     public void addSave(User addUser){
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        addUser.setPassword(encoder.encode(addUser.getPassword()));
+
         String sql = "INSERT INTO usergood(username, password) VALUES (?, ?)";
         jdbcTemplate.update(sql, addUser.getUsername(), addUser.getPassword());
     }
