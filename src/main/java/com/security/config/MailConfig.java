29dbd2aeec79029bb15af66af7f12d9505ec10de
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-02-28 19:20:31 +0500

    Added mail configuration

--- src/main/java/com/security/config/MailConfig.java
+++ src/main/java/com/security/config/MailConfig.java
@@ -1,8 +1,10 @@
 package com.security.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
@@ -11,25 +13,48 @@ import java.util.Properties;
 
 @Configuration
 @ComponentScan("com.security")
+@PropertySource("classpath:mailConfig.properties")
 public class MailConfig {
 
+    @Value("${mail.username}")
     private String username;
+
+    @Value("${mail.password}")
+    private String password;
+
+    @Value("${mail.host}")
+    private String host;
+
+    @Value("${mail.port}")
+    private int port;
+
+    @Value("${mail.properties.protocol}")
+    private String protocol;
+
+    @Value("${mail.properties.auth}")
+    private String auth;
+
+    @Value("${mail.properties.ssl.enable}")
+    private String sslEnable;
+
+    @Value("${mail.properties.debug}")
+    private String debug;
 
     @Bean
     public JavaMailSender javaMailSender(){
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(465);
-        mailSender.setUsername("nizomovasadbekjava@gmail.com");
-        mailSender.setPassword("nylmvqqfohhlznzu");
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties p = new Properties();
 
-        p.put("mail.transport.protocol", "smtp");
-        p.put("mail.smtp.auth", "true");
-        p.put("mail.smtp.ssl.enable", "true");
-        p.put("mail.debug", "true");
+        p.put("mail.transport.protocol", protocol);
+        p.put("mail.smtp.auth", auth);
+        p.put("mail.smtp.ssl.enable", sslEnable);
+        p.put("mail.debug", debug);
 
         mailSender.setJavaMailProperties(p);
 
