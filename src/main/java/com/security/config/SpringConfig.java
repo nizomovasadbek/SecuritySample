0df59df53dfc48cf4fe7fc52864be72a647e3647
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-02-28 19:24:20 +0500

    changed to full properties

--- src/main/java/com/security/config/SpringConfig.java
+++ src/main/java/com/security/config/SpringConfig.java
@@ -1,10 +1,12 @@
 package com.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,9 +17,19 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 @Configuration
 @ComponentScan("com.security")
 @EnableWebMvc
+@PropertySource("classpath:primary.properties")
 public class SpringConfig implements WebMvcConfigurer {
 
     private ApplicationContext applicationContext;
+
+    @Value("${meta.charset}")
+    private String charset;
+
+    @Value("${thymeleaf.prefix}")
+    private String prefix;
+
+    @Value("${thymeleaf.suffix}")
+    private String suffix;
 
     @Autowired
     public SpringConfig(ApplicationContext app){
